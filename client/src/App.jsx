43719d6f5ecf430bd7f3c2fba0a4c8d012da5d64
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-10-22 11:35:53 +0300

    refactor: update App component structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import NavBar from "./pages/Home/Navbar";
 import Footer from "./pages/Home/Footer";
-import NotFound from "./pages/NotFound/NotFound"; // Create a NotFound page component for undefined routes
-
+// import NotFound from "./pages/NotFound/NotFound"; // Create a NotFound page component for undefined routes
+import Blog from "./pages/Blog/Blog";
 export default function App() {
   return (
     <BrowserRouter>
@@ -12,12 +12,13 @@ export default function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
 
         {/* Add more routes here as needed */}
         {/* Example: <Route path="/about" element={<About />} /> */}
 
         {/* Catch-all route for undefined paths */}
-        <Route path="*" element={<NotFound />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
 
       {/* Footer is common across all routes */}
