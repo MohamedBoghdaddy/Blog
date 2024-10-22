987937674b9d4eb505c7d46fcfd4820a987e8c0b
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2024-10-22 11:36:36 +0300

    feat: add routes for blog search and listing in BlogRoutes

--- server/routes/BlogRoutes.js
+++ server/routes/BlogRoutes.js
@@ -4,11 +4,8 @@ import {
   uploadBlog,
   downloadBlog,
   softDeleteBlog,
-  previewBlog,
   listBlogInWorkspace,
   restoreBlog,
-  updateBlogMetadata,
-  updateBlogTags,
   searchBlogs,
 } from "../controller/blogController.js";
 import { auth } from "../Middleware/authMiddleware.js";
@@ -21,9 +18,6 @@ router.put("/:id/soft-delete", auth, softDeleteBlog);
 router.put("/:id/restore", auth, restoreBlog);
 router.get("/:workspaceId/blogs", auth, listBlogInWorkspace);
 router.get("/download/:id", auth, downloadBlog);
-router.get("/preview/:id", auth, previewBlog);
-router.put("/:id/metadata", auth, updateBlogMetadata);
-router.put("/:id/tags", auth, updateBlogTags);
 router.get("/search", auth, searchBlogs);
 
 export default router;
