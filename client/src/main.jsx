5d332b82fc7cff492f8e637519c1861a895eff88
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2025-02-07 17:55:21 +0200

    wrapped providers

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,23 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { store, persistor } from "./redux/store.js";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { Provider } from "react-redux";
-import{PersistGate} from 'redux-persist/integration/react';
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { AuthProvider } from "./context/AuthContext";
+import DashboardProvider from "./context/DashboardContext"; // ✅ Added DashboardProvider
+import "./index.css"; // ✅ Ensures styles are included
 
-createRoot(document.getElementById('root')).render(
- <PersistGate persistor={persistor}>
- <Provider store = {store}>
-    <App />
-  </Provider>
-  </PersistGate>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <AuthProvider>
+          <DashboardProvider>
+              <App />
+          </DashboardProvider>
+        </AuthProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
