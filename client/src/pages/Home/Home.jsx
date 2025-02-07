43bb8d7a8e66deb3acc67c03c7250916b6a8a172
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2025-02-07 17:50:59 +0200

    refactor: improve Home page structure

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,23 +1,13 @@
 import HeroSection from "./HeroSection";
-import BlogList from "./BlogList";
-import FeaturedBlog from "./FeaturedBlogs"
 import PopularTags from "./PopularTags";
 import SubscribeSection from "./SubscribeSection";
 
 const Home = () => {
   return (
-    <div className="full-page">
-      <section id="hero-section">
-        <HeroSection />
-      </section>
-        <BlogList/>
-      <section id="WhoWeAre">
-        <FeaturedBlog/>
-      </section>
-      <section id="contact">
-        <PopularTags/>
-      </section>
-        <SubscribeSection/>
+    <div>
+      <HeroSection />
+      <PopularTags />
+      <SubscribeSection />
     </div>
   );
 };
