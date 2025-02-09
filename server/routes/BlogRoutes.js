ccb837fee94c3ef998a76b63c115d467282aee67
Mohamed Hesham <mohamedhammad3.142@gmail.com>
2025-02-09 07:59:33 +0200

    Added proper routing for blog-related operations. Applied middleware to protect routes.

--- server/routes/BlogRoutes.js
+++ server/routes/BlogRoutes.js
@@ -1,23 +1,25 @@
 import express from "express";
-import multer from "multer";
 import {
-  uploadBlog,
-  downloadBlog,
-  softDeleteBlog,
-  listBlogInWorkspace,
-  restoreBlog,
+  createBlog,
+  getAllBlogs,
+  getBlogById,
+  updateBlog,
+  deleteBlog,
+  likeBlog,
+  addComment,
   searchBlogs,
 } from "../controller/blogController.js";
 import { auth } from "../Middleware/authMiddleware.js";
 
 const router = express.Router();
-const upload = multer({ dest: "uploads/" });
 
-router.post("/upload", auth, upload.single("document"), uploadBlog);
-router.put("/:id/soft-delete", auth, softDeleteBlog);
-router.put("/:id/restore", auth, restoreBlog);
-router.get("/:workspaceId/blogs", auth, listBlogInWorkspace);
-router.get("/download/:id", auth, downloadBlog);
-router.get("/search", auth, searchBlogs);
+router.post("/", auth, createBlog);
+router.get("/", getAllBlogs);
+router.get("/search", searchBlogs);
+router.get("/:id", getBlogById);
+router.put("/:id", auth, updateBlog);
+router.delete("/:id", auth, deleteBlog);
+router.post("/:id/like", auth, likeBlog);
+router.post("/:id/comments", auth, addComment);
 
 export default router;
